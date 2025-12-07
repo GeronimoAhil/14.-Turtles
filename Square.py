e7378ea58c0355b3ba47e6f12ec43c69e3df5d55
GeronimoAhil <geromy2014@gmail.com>
2025-12-07 19:16:30 +0800

    Add files via upload

---
+++ Square.py
@@ -0,0 +1,27 @@
+import turtle
+turtle.Screen().bgcolor("Teal")
+turtle.Screen().setup(400,400)
+
+square=turtle.Turtle()
+square.forward(100)
+square.right(90)
+square.forward(100)
+square.right(90)
+square.forward(100)
+square.right(90)
+square.forward(100)
+square.backward(50)
+square.right(30)
+square.forward(100)
+square.right(120)
+square.forward(100)
+square.right(60)
+square.forward(100)
+square.right(120)
+square.forward(100)
+
+
+
+turtle.done()
+
+
